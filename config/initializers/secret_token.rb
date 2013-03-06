00b9d6462046dc5286c5ec79649171fea69ecb62
Jordan Hatch <jordan@jordanh.net>
2013-03-06 15:18:51 +0000

    Make it obvious that the secret token should be replaced

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TransactionWrappers::Application.config.secret_token = '06f3bf243f4ec377da33b82b940bc28cff5ccb7b2189877606e76ef61d8b68f4cc192b041f6153b4bca89dfc218e9b935d48d741014f126fde018f428c81f156'
+TransactionWrappers::Application.config.secret_token = 'replace this in production with something that is actually secret'
