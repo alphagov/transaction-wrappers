62b4931ef7d89b64048c4137a221b9d97809c3fe
Jordan Hatch <jordan@jordanh.net>
2013-03-13 17:31:36 +0000

    Make error_404 a private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include ApplicationHelper
 
-  def error_404; error 404; end
-
   private
+
+  def error_404; error 404; end
 
   def error(status_code)
     render status: status_code, text: "#{status_code} error"
