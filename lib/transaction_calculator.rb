c0a09a3de3e2325a85bc0dd345594777905427f5
Jordan Hatch <jordan@jordanh.net>
2013-03-18 16:02:42 +0000

    Tidy up TransactionCalculator

--- lib/transaction_calculator.rb
+++ lib/transaction_calculator.rb
@@ -11,28 +11,29 @@ class TransactionCalculator
     document_type = values[:document_type]
     postage_option = values[:postage_option]
 
-    item_list = []
-
+    item_list = Hash.new("")
     document_total = @transaction.document_cost * document_count
 
-    if @transaction.postage_options
-      postage_option = @transaction.postage_options[postage_option]
+    if @transaction.postage_options.present?
+      if postage_option.present?
+        postage_method = @transaction.postage_options[postage_option]
+      end
+      raise Transaction::InvalidPostageOption unless postage_method
 
-      raise Transaction::InvalidPostageOption unless postage_option
-
-      postage_total = postage_option['cost']
-    else
-      postage_total = postage ? @transaction.postage_cost : 0
+      postage_total = postage_method['cost']
+      item_list[:postage] = ", plus #{postage_method['label']} postage,"
+    elsif postage
+      postage_total = @transaction.postage_cost
+      item_list[:postage] = ", plus postage,"
     end
-
-    total_cost = document_total + postage_total
+    total_cost = document_total + (postage_total || 0)
 
     if @transaction.registration
       registration_count = values[:registration_count].to_i
       registration_total = @transaction.registration_cost * registration_count
       total_cost += registration_total
 
-      item_list << "#{registration_count} " + pluralize_document_type_label(registration_count, "#{@transaction.registration_type} registration") + " and "
+      item_list[:registration] = "#{registration_count} " + pluralize_document_type_label(registration_count, "#{@transaction.registration_type} registration") + " and "
       document_type_label = "#{@transaction.registration_type} certificate"
     end
 
@@ -42,16 +43,13 @@ class TransactionCalculator
       end
       raise Transaction::InvalidDocumentType unless document_type_label
     end
+    item_list[:document] = "#{document_count} " + pluralize_document_type_label(document_count, document_type_label || "document")
 
-    item_list << "#{document_count} " + pluralize_document_type_label(document_count, document_type_label || "document")
-
-    if postage_option
-      item_list << ", plus #{postage_option['label']} postage,"
-    elsif postage
-      item_list << ", plus postage,"
-    end
-
-    return OpenStruct.new(:total_cost => total_cost, :item_list => item_list.join(''))
+    item_list_order = [:registration, :document, :postage]
+    return OpenStruct.new(
+      :total_cost => total_cost,
+      :item_list => item_list_order.map {|key| item_list[key] }.join("")
+    )
   end
 
   private
