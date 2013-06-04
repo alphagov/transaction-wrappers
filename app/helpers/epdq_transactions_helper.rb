a88cdbc4f0a9cbf31c7c6ce06a753caf1c320bc1
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2013-06-04 10:25:35 +0100

    Revert "Revert "Merge pull request #19 from alphagov/instrument-user-journey""
    Issue that caused the revert was fixed in e9e87066538e8b4d435aa62aee7ed1be069d6c11
    
    This reverts commit e29e96a089edbe0ef65fe91e6e6594df4c77eba5.

--- app/helpers/epdq_transactions_helper.rb
+++ app/helpers/epdq_transactions_helper.rb
@@ -15,6 +15,10 @@ module EpdqTransactionsHelper
     end.join(" and ")
   end
 
+  def data_journey
+    "data-journey=#{@journey_description}" if @journey_description.present?
+  end
+
   private
 
   def document_count
