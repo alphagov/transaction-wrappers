2c6fed24a73ccdf8e7d88b02c0f9bf6ae6ad6b1d
Lee Longmore <lee@commentarybox.co.uk>
2013-09-20 17:09:30 +0100

    Redirect GET requests to /confirm to /start

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ TransactionWrappers::Application.routes.draw do
   get "/:slug" => "epdq_transactions#show"
   get "/:slug/start" => "epdq_transactions#start", :as => :transaction
   post "/:slug/confirm" => "epdq_transactions#confirm", :as => :transaction_confirm
+  get "/:slug/confirm" => redirect("/%{slug}/start"), :format => false
   get "/:slug/done" => "epdq_transactions#done", :as => :transaction_done
 
   root :to => "epdq_transactions#index"
